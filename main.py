a393efa74dd5fe3a495e6cf8c4ffd6bde80a282a
dCrowb <oishevchenko94@gmail.com>
2023-09-19 21:17:28 +0300

    finish

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+from src.routes import contacts, users
+
 app = FastAPI()
 
-@app.get('/api/healthchecker')
+app.include_router(contacts.router, prefix='/api')
+app.include_router(users.router, prefix='/api')
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/api/healthchecker")
 def root():
-    return {'message': 'Welcome to FastAPI'}
+    return {"message": "Welcome to FastAPI!"}
